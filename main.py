36794b95c413db836f83744c2e289291074a4340
Matheus <matheushnt06@gmail.com>
2024-06-19 20:36:39 -0300

    Exercício concluído

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from exercicios.exer002 import Livro
+from exercicios.exer003 import Funcionario
 
 # Arquivo criado para testar a funcionalidade das classes
 
-livro1 = Livro(titulo='Duna', autor='Frank Herbert', paginas=544)
-livro2 = Livro(titulo='Sun Tzu ', autor='A Arte da Guerra', paginas=160)
-livro3 = Livro(titulo='Guerra do Velho', autor='John Scalzi', paginas=368)
+func_matheus = Funcionario(nome='Matheus', salario=1000)
 
-livro1.exibir_detalhes()
-livro2.exibir_detalhes()
-livro3.exibir_detalhes()
-print(f'Quantidade de Livros: {Livro.total_livros()}')
+print(func_matheus.salario)
+func_matheus.salario = -1000
+
+print(func_matheus.salario)
+
+func_matheus.salario = 2500
+print(func_matheus.salario)    # Novo salário
