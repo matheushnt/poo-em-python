dc14bed5ed3be537d92af0f67e02dc9baf3ffecb
Matheus <matheushnt06@gmail.com>
2024-07-11 17:05:59 -0300

    Testes realizados

--- testes.py
+++ testes.py
@@ -1,32 +1,51 @@
 import unittest
-from exercicios.exer010 import Empresa, Funcionario
+from exercicios.exer011 import (Funcionario,
+                                Gerente, Desenvolvedor, Estagiario)
 
 
-class TestEmpresa(unittest.TestCase):
+class TestFuncionario(unittest.TestCase):
     def setUp(self):
-        self.empresa = Empresa('Math Motors')
-        self.func1 = Funcionario('Ana')
-        self.func2 = Funcionario('Bob')
+        self.funcionario = Funcionario('Bob', 30, 3000)
 
-    def test_adicionar_funcionario(self):
-        self.empresa.adicionar_funcionario(self.func1)
-        self.assertIn('Ana', self.empresa.listar_funcionarios())
+    def test_exibir_detalhes_funcionario(self):
+        self.assertEqual(self.funcionario.exibir_detalhes(),
+                         'Nome: Bob, Idade: 30 anos, Salário: R$3000.00')
 
-    def test_nao_adicionar_funcionario_nao_instanciado(self):
-        self.empresa.adicionar_funcionario('Carlos')
-        self.assertNotIn('Carlos', self.empresa.listar_funcionarios())
 
-    def test_remover_funcionario(self):
-        self.empresa.adicionar_funcionario(self.func1)
-        self.empresa.adicionar_funcionario(self.func2)
-        self.empresa.remover_funcionario('Bob')
-        self.assertIn('Ana', self.empresa.listar_funcionarios())
-        self.assertNotIn('Bob', self.empresa.listar_funcionarios())
+class TestGerente(unittest.TestCase):
+    def setUp(self):
+        self.gerente = Gerente('Gil', 38, 8000, 'TI')
 
-    def test_listar_funcionarios(self):
-        self.empresa.adicionar_funcionario(self.func1)
-        self.empresa.adicionar_funcionario(self.func2)
-        self.assertEqual(['Ana', 'Bob'], self.empresa.listar_funcionarios())
+    def test_calcular_bonus_gerente(self):
+        self.assertEqual(self.gerente.calcular_bonus(), 1600)
+
+    def test_exibir_detalhes_gerente(self):
+        self.assertEqual(self.gerente.exibir_detalhes(),
+                         'Nome: Gil, Idade: 38 anos, Salário: R$8000.00')
+
+
+class TestDesenvolvedor(unittest.TestCase):
+    def setUp(self):
+        self.desenvolvedor = Desenvolvedor('Ana', 25, 5000, 'Python')
+
+    def test_calcular_bonus_desenvolvedor(self):
+        self.assertEqual(self.desenvolvedor.calcular_bonus(), 600)
+
+    def test_exibir_detalhes_desenvolvedor(self):
+        self.assertEqual(self.desenvolvedor.exibir_detalhes(),
+                         'Nome: Ana, Idade: 25 anos, Salário: R$5000.00')
+
+
+class TestEstagiario(unittest.TestCase):
+    def setUp(self):
+        self.estagiario = Estagiario('Mat', 19, 1800, 'Estácio')
+
+    def test_calcular_bonus_estagiario(self):
+        self.assertEqual(self.estagiario.calcular_bonus(), 144)
+
+    def test_exibir_detalhes_estagiario(self):
+        self.assertEqual(self.estagiario.exibir_detalhes(),
+                         'Nome: Mat, Idade: 19 anos, Salário: R$1800.00')
 
 
 if __name__ == '__main__':
